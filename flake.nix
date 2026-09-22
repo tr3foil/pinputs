36aacad6e4b8ccf1975263c3dfa0572379c12060
Clover Ison <clover@isons.org>
2026-09-22 15:44:52 -0700

    refactor: move nixos module into it's own file

--- flake.nix
+++ flake.nix
@@ -3,31 +3,7 @@
 description = "Pin your NixOS flake inputs to the system flake registry";
 
 outputs = { self }: {
-  nixosModules.default = { config, lib, ... }: {
-    options.pins = {
-      inputs = lib.mkOption {
-        type = lib.types.attrsOf lib.types.anything;
-        description = lib.mdDoc "The set of inputs to pin";
-      };
-    };
-
-    config = {
-      nix.registry = let
-        mkPin = name: value: { ${name}.flake = value; };
-      in lib.concatMapAttrs mkPin config.pins.inputs;
-
-      nix.nixPath = let
-        mkPin = name: _: "${name}=flake:${name}";
-      in lib.mapAttrsToList mkPin config.pins.inputs;
-
-      assertions = [
-        {
-          assertion = lib.all (lib.isType "flake") (lib.attrValues config.pins.inputs);
-          message = "'pins.inputs' needs to be an attribute set of flakes";
-        }
-      ];
-    };
-  };
+  nixosModules.default = import ./module.nix;
 };
 
 }
---
+++ module.nix
@@ -0,0 +1,27 @@
+{ config, lib, ... }: {
+
+options.pins = {
+  inputs = lib.mkOption {
+    type = lib.types.attrsOf lib.types.anything;
+    description = lib.mdDoc "The set of inputs to pin";
+  };
+};
+
+config = {
+  nix.registry = let
+    mkPin = name: value: { ${name}.flake = value; };
+  in lib.concatMapAttrs mkPin config.pins.inputs;
+
+  nix.nixPath = let
+    mkPin = name: _: "${name}=flake:${name}";
+  in lib.mapAttrsToList mkPin config.pins.inputs;
+
+  assertions = [
+    {
+      assertion = lib.all (lib.isType "flake") (lib.attrValues config.pins.inputs);
+      message = "'pins.inputs' needs to be an attribute set of flakes";
+    }
+  ];
+};
+
+}
