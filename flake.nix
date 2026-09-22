ab08ac81dec2df9f4bafbcca27370563812e42ac
Clover Ison <clover@isons.org>
2026-09-22 13:59:35 -0700

    chore(flake): avoid `with lib` antipattern

--- flake.nix
+++ flake.nix
@@ -3,26 +3,26 @@
 description = "Pin your NixOS flake inputs to the system flake registry";
 
 outputs = { self }: {
-  nixosModules.default = { config, lib, ... }: with lib; {
+  nixosModules.default = { config, lib, ... }: {
     options.pins = {
-      inputs = mkOption {
-        type = types.attrsOf types.anything;
-        description = mdDoc "The set of inputs to pin";
+      inputs = lib.mkOption {
+        type = lib.types.attrsOf lib.types.anything;
+        description = lib.mdDoc "The set of inputs to pin";
       };
     };
 
     config = {
       nix.registry = let
         mkPin = name: value: { ${name}.flake = value; };
-      in concatMapAttrs mkPin config.pins.inputs;
+      in lib.concatMapAttrs mkPin config.pins.inputs;
 
       nix.nixPath = let
         mkPin = name: _: "${name}=flake:${name}";
-      in mapAttrsToList mkPin config.pins.inputs;
+      in lib.mapAttrsToList mkPin config.pins.inputs;
 
       assertions = [
         {
-          assertion = all (isType "flake") (attrValues config.pins.inputs);
+          assertion = lib.all (lib.isType "flake") (lib.attrValues config.pins.inputs);
           message = "'pins.inputs' needs to be an attribute set of flakes";
         }
       ];
